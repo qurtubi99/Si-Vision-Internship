aabaeb98883184ec987957aefbd5e6e7a05c6a73
Ezz Al-Qurtubi <ezz.alqurtubi@gmail.com>
2024-01-23 07:45:50 +0200

    Update S1 & S2

--- Eclipse_Wks/S1_Fundementals_Of_Cpp/src/S1_Fundementals_Of_Cpp.cpp
+++ Eclipse_Wks/S1_Fundementals_Of_Cpp/src/S1_Fundementals_Of_Cpp.cpp
@@ -7,24 +7,150 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
 
-//Return by reference feature in cpp
-int& accumlate (int x, int y) {
+// Versions of C++ C++98 >> C++3 >> C++11 >+3> C++14 >+3> C++17 >+3> C++20 >+3> C++23
+
+/****************************************************************************************************************************************/
+//0. Differnece between NULL in c and nullptr in CPP:
+// 	null in c is ((void*)0) so I can say x = NULL + 1; and now x = 1;
+// 	nullptr in cpp is:
+//		- built in (no need to include iosteram)
+//		- Can only be used with pointer (otherwise compiler error)
+//		- namespace std has nullptr_t type (I can say nullptr_t a = nullptr)
+
+/****************************************************************************************************************************************/
+//1. Local vs Global Variables
+/*
+  Local Variables:
+	1. Functions’ formal parameters and defined variables.
+	2. Scope: Function scope/ Block scope
+	3. Lifetime: Function Lifetime
+   Global Variables:
+	1. Variables are defined outside any block (function, class, struct).
+	2. Scope: File Scope
+	3. Lifetime: Whole Program
+ */
+
+/****************************************************************************************************************************************/
+//2. Static Keyword:
+/*
+	- Static variables keep their values and are not destroyed even after they go out of scope
+	- Initialized only once
+	- Still Local to this function
+ */
+void func2() {
+	static int callCount = 0;
+	std::cout<<callCount++<<"\n";
+}
+
+/****************************************************************************************************************************************/
+//3. L-Values vs R-Values:
+/*
+	- L-Values are objects that occupies some identifiable location in memory
+	- R-Values are object that have no identifiable memory location
+ */
+int i = 10; 		// "i" has an identifiable memory location, while 10 is not "R-value"
+int *ptr = &i;		// "i" can be modified
+//int i = sum(10,1);	// “sum(10,1)” has no memory location, i.e. its value cannot be modified.
+
+/****************************************************************************************************************************************/
+//4. Reference:
+/*
+	- Reference variables don’t allocate new memory space, but share the same memory location of the RHS variable
+	- Better than pointer:
+		- Differentiate between ptr to int and to array of int (syntax difference)
+		- No need to check against null (less complex code)
+		- No ownership problem (delete the passed pointer after using it if it points to dynamically allocated space in heap or not)
+ */
+//const gurantee that if R-value is passed, Its temporary memory location will not be destroyed till the end of the function.
+int& accumlate (const int &x,const int &y) {
 	static int accumlator = 0;
 	accumlator += x + y;
 	return accumlator;
 }
+void func4(int *&param){
+	std::cout<<*param<<"\n";
+}
+void foo(int (&x)[10]){
+	std::cout<< "Element 5 in the passed array by reference: "<<x[4]<<"\n";
+}
+
+/****************************************************************************************************************************************/
+//5. Difference between passing L & R-value to function:
+/*
+	L-value: just copy passed variable to function argument
+		void copyString(string str);
+		string testString = "Test";
+		copyString(testString);
+	R-value: Create a temporary variable >>  Copy its content to str >> Destroy the temporary variable
+		void copyString(string str);
+		copyString("Literals");
+
+		void copyString(string& str);
+		copyString("Test"); >>This will give compiler error cuz the temp memroy location will be destroyed when passed to function
+							..So I must use const keyword
+ */
+
+/****************************************************************************************************************************************/
+//6. new and delete Operators:
+/*
+	string *pointer1 = new string();
+	string *pointer2 = new string[10];
+	delete pointer1;
+ */
+
+/****************************************************************************************************************************************/
+//7. Procedural [POP] vs Object oriented programming [OOP]:
+/*
+	POP:
+		- App = functions.
+		- Problem: data members are not specific for certain functions, and any function in any module can use it.
+	OOP:
+		- Solve this problem and gurantee more maintainale and optimized design
+		- class == struction template, no memory reserved till an object/instance is created.
+		- Encapsulation: method members and data members in same class
+		- Abstraction: If app is big, divide it in to multiple classes, then create object with instances of all required classes.
+ */
+
+/****************************************************************************************************************************************/
+
 
 int main() {
-	int &x = accumlate(1,2);		// x is now 3
-	cout << x << endl;
-	x++;							// x is now 4
-	cout << x << endl;
+	/****************************************************************************************************************************************/
+	//2. Static Keyword:
+	{
+		std::cout<<"2. Static Keyword: \n";
+		func2();
+		func2();
+		func2();
+	}
 
-	int &y = accumlate(0,1);		// x is now 5
-	cout << x << endl;
-	y++;							// x is now 6
-	cout << x << endl;
+	/****************************************************************************************************************************************/
+	//4. Reference:
+	{
+		std::cout<<"4. Reference accumlate calling: \n";
+		int &x = accumlate(1,2);		// x is now 3
+		std::cout << x << std::endl;
+		x++;							// x is now 4
+		std::cout << x << std::endl;
+
+		int &y = accumlate(0,1);		// x is now 5
+		std::cout << x << std::endl;
+		y++;							// x is now 6
+		std::cout << x << std::endl;
+
+		//Passing pointer by reference
+		std::cout<<"4. Reference func4 calling: \n";
+		int z1 = 20;
+		int *z2 = &z1;
+		func4(z2);
+
+		//Passing array by reference
+		std::cout<<"4. Reference foo calling: \n";
+		int x1[10] = {1,2,3,4,13,6};
+		foo(x1);
+	}
+
+	/****************************************************************************************************************************************/
 	return 0;
 }
--- Eclipse_Wks/S2_Basics_Of_OOP/src/S2_Basics_Of_OOP.cpp
+++ Eclipse_Wks/S2_Basics_Of_OOP/src/S2_Basics_Of_OOP.cpp
@@ -10,8 +10,244 @@
 #include <string.h>
 using namespace std;
 
-/**********************************************************************************************/
-//One line used to calculate the execution time of a function
+/****************************************************************************************************************************************/
+//1. Classes, access specifiers, Constructors(defalut, parametrized & copy), destructor, setters, getters, RVO & delegating constructors:
+//	- Access Specifiers/Modifiers:
+//		- Public: Data and function members can be accessed outside the class
+//		- Private: Data and function members can not to be accessed outside the class. Friend functions and class can only access them.
+//		- Protected: Data and function members are can not be accessed outside the class but they can be accessed by any derived classes (inheritance)
+class MyString{
+private:
+	//Data members
+	int size;
+	char *literals;
+public:
+	//Method members
+	//Default constructor (no i/p arguments/parameters)
+	MyString(){
+		cout << "Default constructor of MyString is called\n";
+
+		//1. Initialize member variables
+		literals = nullptr;
+		size = 0;
+
+		//2. do any setup steps necessary for the class to be used.
+	}
+
+	//Parametrized constructor takes pointer to character
+	//- inputLiterals must be defined const, to delay destruction of temporary memory location allocated by the compiler for the passed
+	//	R-value string till the complete implementation for the parametrized constructor
+	MyString(const char *inputLiterals){
+		cout << "Parametrized constructor of MyString is called\n";
+		size = strlen(inputLiterals);
+		literals = new char[size + 1];			//+1 for '\0'
+		memcpy(literals,inputLiterals,size);
+		literals[size] = '\0';
+
+	}
+
+	//Copy constructor defined in this way (const and pass by reference) ; cuz the compiler define it in the same way
+	//The compiler can perform copy elision by implementing Return Value Optimization (RVO)
+	// When is the copy constructor called?
+	//	- When an object of the class is returned by value. [create temp class, copy the data in it then return it]
+	//	- When an object of the class is passed (to a function) by value as an argument
+	//	- When an object is constructed based on another object of the same class
+	//	- When the compiler generates a temporary object
+	/*
+	 	// Without RVO
+		std::string f() {
+		  std::string s = "Hello";
+		  return s; // copy s to a temporary object
+		}
+
+		std::string t = f(); // copy the temporary object to t
+
+		// With RVO
+		std::string f() {
+		  std::string s = "Hello";
+		  return s; // no copy, s is constructed directly into the storage of the return value
+		}
+
+		std::string t = f(); // no copy, t is initialized from the return value
+
+	 */
+	//copy constructor takes MyString object by reference
+	MyString(const MyString &other):
+		size(other.size),
+		literals(new char[size + 1])
+	{
+		cout << "Copy constructor of MyString is called\n";
+		memcpy(literals, other.literals, size);
+		literals[size] = '\0';
+	}
+
+	//Since the copy constructor makes same things in parametrized constuctor, I may delegate it as follows
+	/*
+	 	MyString(const MyString &other): MyString(other.literals)
+		{
+			cout << "Copy constructor is called\n";
+		}
+	 */
+
+
+	void print(){
+		cout << "Literals: " << literals << endl;
+	}
+
+	//Getter for the size
+	//const after getSize() gureantees that the data member I return must be read only, and give error, if I've returned it by reference
+	//	or modified it in the getter function.
+	int getSize() const{
+		return size;
+	}
+
+	//Setter for literals
+	void setLiterals(const char *inputLiterals){
+		int newSize = strlen(inputLiterals);
+		if(newSize > size){
+			//Through exception
+			//return error
+		}else{
+			memcpy(literals,inputLiterals,size);
+		}
+	}
+
+	//Destructor
+	//- Deallocate object from memory location
+	//- called when stack-allocated objects go out of scope or when delete is called on heap-allocated objects.
+	~MyString(){
+		//Make cleanup operations (data | logic)
+		delete []literals;			//De-allocate the used heap locations throughout the object lifetime
+		size = 0;
+	}
+};
+
+/****************************************************************************************************************************************/
+//2. What exactly should I do in constructors and destructors?
+class Database{
+public:
+	Database(){
+		//1. Initialize member variables
+		//2. do any setup steps necessary for the class to be used
+		//	2.1 Discover the server.
+		//	2.2 Connect to server
+		//	2.3 Authentication
+	}
+
+	~Database(){
+		//Make cleanup operations (data | logic)
+		//- Make opposit steps made in constructor.
+	}
+};
+
+/****************************************************************************************************************************************/
+//3. Parametrized constructor with initial value:
+class Dummy{
+private:
+	int x;
+public:
+	//Parametrized constructor with initial value.
+	Dummy(int xInput = 15){
+		cout<<"Parametrized constructor in Dummy class.\n";
+		x = xInput;
+	}
+};
+
+/****************************************************************************************************************************************/
+//4. Initializer list with constructors:
+
+class Employee{
+private:
+	MyString myStringObj;
+public:
+
+	/*Default constuctor rules:
+		– Constructors must have the same name as the class
+		– Constructors have no return type (not even void)
+		– Constructors can be overloaded. [1. Default constructor 2. Parametrized constructor 3. Copy constructor...]
+		– Constructors can take default arguments
+		– If it’s not defined, then compiler will implicitly define a default one.
+	 */
+	Employee(){
+
+	}
+
+	//Parametrized constructor
+	//	- If the id parameter was received by reference (int &id), it should have been defined
+	//		as constant incase the employee object was defined with passing R-value id (e.g. Employee employee(5);)
+	Employee(int id){
+		cout<<"Parametrized constructor in Employee class.\n";
+		if(id == 0){
+			myStringObj = "Ahmed";
+		}else{
+			myStringObj = "Mohamed";
+		}
+	}
+};
+
+/****************************************************************************************************************************************/
+//5. Expicit keyword with constructors:
+
+class Dummy2{
+private:
+	int x;
+
+public:
+	explicit Dummy2(int x):x(x){
+		cout<<"Parametrized constructor in Dummy2 class.\n";
+	}
+};
+
+/****************************************************************************************************************************************/
+//Example to explain delegation in calsses:
+
+class Database2{
+public:
+	//Delegating the parametrized constructor.
+	Database2():Database2("root","hello123"){
+		//1. Initialize member variables
+		//2. do any setup steps necessary for the class to be used
+		//	2.1 Discover the server.
+		//	2.2 Connect to server
+		//	2.3 Authentication
+		cout<<"Default constructor in Database2 class.\n";
+	}
+
+	Database2(const string &userName,const string &password){
+		cout<<"Parametrized constructor in Database2 class.\n";
+		//1. Initialize member variables
+		//2. do any setup steps necessary for the class to be used
+		//	2.1 Discover the server.
+		DiscoverServer();
+		//	2.2 Connect to server
+		ConnectServer();
+		//	2.3 Authentication
+		Authenticate(userName,password);
+
+	}
+	~Database2(){
+		cout<<"Destructor in Database2 class.\n";
+		//Make cleanup operations (data | logic)
+		//- Make opposit steps made in constructor.
+	}
+
+	void DiscoverServer(){
+
+	}
+	void ConnectServer(){
+
+	}
+	void Authenticate(const string &userName,const string &password){
+
+	}
+};
+
+void func(Dummy2 dummy2Obj){
+
+}
+
+/****************************************************************************************************************************************/
+// Interview Question: One line used to calculate the execution time of a function
 
 struct Time {
 	int hours, minutes, seconds;
@@ -47,222 +283,9 @@ public:
 void func(){
 	Execution_Time execution_TimeObject;
 }
-/**********************************************************************************************/
-//Example on class with Constructors(defalut, parametrized & copy), destructor, setters and getters:
-
-class MyString{
-	//Data members
-private:
-	int size;
-	char *literals;
-	//Method members
-public:
-	//Default constructor (no i/p arguments/parameters)
-	MyString(){
-		cout << "Default constructor of MyString is called\n";
-
-		//1. Initialize member variables
-		literals = nullptr;
-		size = 0;
-
-		//2. do any setup steps necessary for the class to be used.
-	}
-
-	//Parametrized constructor
-	//- inputLiterals must be defined const, to delay destruction of temporary memory location
-	//	allocated by the compiler for the passed string till the complete implementation for
-	//	the parametrized constructor
-	MyString(const char *inputLiterals){
-		cout << "Parametrized constructor of MyString is called\n";
-		size = strlen(inputLiterals);
-		literals = new char[size + 1];			//+1 for '\0'
-		memcpy(literals,inputLiterals,size);
-		literals[size] = '\0';
-
-	}
-
-	//I must define the copy constructor in this way (const and pass by reference) ; cuz the compiler define it in the same way
-	//The compiler can perform copy elision by implementing Return Value Optimization (RVO)
-	// When is the copy constructor called?
-	//	- When an object of the class is returned by value.
-	//	- When an object of the class is passed (to a function) by value as an argument
-	//	- When an object is constructed based on another object of the same class
-	//	- When the compiler generates a temporary object
-	/*
-	 	// Without RVO
-		std::string f() {
-		  std::string s = "Hello";
-		  return s; // copy s to a temporary object
-		}
-
-		std::string t = f(); // copy the temporary object to t
-
-		// With RVO
-		std::string f() {
-		  std::string s = "Hello";
-		  return s; // no copy, s is constructed directly into the storage of the return value
-		}
-
-		std::string t = f(); // no copy, t is initialized from the return value
-
-	 */
-	MyString(const MyString &other):
-		size(other.size),
-		literals(new char[size + 1])
-	{
-		cout << "Copy constructor is called\n";
-		memcpy(literals, other.literals, size);
-		literals[size] = '\0';
-	}
-	//Since the copy constructor makes same things in parametrized constuctor, I may delegate it as follows
-	/*
-	 	MyString(const MyString &other): MyString(other.literals)
-		{
-			cout << "Copy constructor is called\n";
-		}
-	 */
-
-
-	void print(){
-		cout << "Literals: " << literals << endl;
-	}
-
-	//Getter for the size
-	//const after getSize() gureantees that the data member I return must be read only, and give error, if
-	// I've returned it by reference.
-	int getSize() const{
-		return size;
-	}
-
-	//Setter for literals
-	void setLiterals(const char *inputLiterals){
-		int newSize = strlen(inputLiterals);
-		if(newSize > size){
-			//Through exception
-			//return error
-		}else{
-			memcpy(literals,inputLiterals,size);
-		}
-	}
-
-	//Destructor
-	//- Deallocate object from memory location
-	//- called when stack-allocated objects go out of scope & when delete is called on heap-allocated objects.
-	~MyString(){
-		//Make cleanup operations (data | logic)
-		delete []literals;			//De-allocate the used heap locations throughout the object lifetime
-		size = 0;
-	}
-};
-/**********************************************************************************************/
-//Example to expalin what should I do in constructors and destrucors.
-class Database{
-public:
-	Database(){
-		//1. Initialize member variables
-		//2. do any setup steps necessary for the class to be used
-		//	2.1 Discover the server.
-		//	2.2 Connect to server
-		//	2.3 Authentication
-	}
-
-	~Database(){
-		//Make cleanup operations (data | logic)
-		//- Make opposit steps made in constructor.
-	}
-};
-
-/**********************************************************************************************/
-//Example to explain the initial value of constructor parameters:
-
-class Dummy{
-private:
-	int x;
-public:
-	//Parametrized constructor with initial value.
-	Dummy(int xInput = 15){
-		x = xInput;
-	}
-};
-
-/**********************************************************************************************/
-//Exaple to expalin the need of initializer list:
-
-class Employee{
-private:
-	MyString myStringObj;
-public:
-	//Parametrized constructor
-	//- If the id parameter was received by reference (int &id), it should have beed defined
-	//	as constant incase the employee object was defined with passing R-value id (e.g. Employee employee(5);)
-	Employee(int id){
-		if(id == 0){
-			myStringObj = "Ahmed";
-		}else{
-			myStringObj = "Mohamed";
-		}
-	}
-};
-
-/**********************************************************************************************/
-//Exaple to explain the used of expicit keyword with constructors:
-
-class Dummy2{
-private:
-	int x;
-
-public:
-	explicit Dummy2(int x):x(x){
-
-	}
-};
-
-/**********************************************************************************************/
-//Example to explain delegation in calsses:
-
-class Database2{
-public:
-	//Delegating the parametrized constructor.
-	Database2():Database2("root","hello123"){
-		//1. Initialize member variables
-		//2. do any setup steps necessary for the class to be used
-		//	2.1 Discover the server.
-		//	2.2 Connect to server
-		//	2.3 Authentication
-	}
-
-	Database2(const string &userName,const string &password){
-		//1. Initialize member variables
-		//2. do any setup steps necessary for the class to be used
-		//	2.1 Discover the server.
-		DiscoverServer();
-		//	2.2 Connect to server
-		ConnectServer();
-		//	2.3 Authentication
-		Authenticate(userName,password);
-
-	}
-	~Database2(){
-		//Make cleanup operations (data | logic)
-		//- Make opposit steps made in constructor.
-	}
-
-	void DiscoverServer(){
-
-	}
-	void ConnectServer(){
-
-	}
-	void Authenticate(const string &userName,const string &password){
-
-	}
-};
-
-void func(Dummy2 dummy2Obj){
-
-}
-
+/****************************************************************************************************************************************/
 int main() {
+	/****************************************************************************************************************************************/
 	MyString teacherName("Ezz");				//Parametrized constructor is called.
 	MyString studentName1;						//Default constructor is called
 	MyString studentName(teacherName);			//Here copy constructor is called and make deep copy
@@ -277,21 +300,25 @@ int main() {
 
 	MyString *namePtr = new MyString("ezz");	//Allocate object dynamically in heap
 
-	Dummy dummyObj(10);							//If I haven't sent 10 to initialize x,
-	//	initial value (15) in parametrized constructor will be taken
+	/****************************************************************************************************************************************/
+	//3. Parametrized constructor with initial value:
+	Dummy dummyObj(10);			//If I haven't sent 10 to initialize x, initial value (15) in parametrized constructor will be taken
 
+	/****************************************************************************************************************************************/
+	//4. Initializer list with constructors:
 	Employee employeeObj(0);
-	//1st jump to Employee(int id)
-	//2nd create data members of Employee class, which will jump to MyString() (Default constructor)
-	//3rd start code of Employee(int id), and when we say myStringObj = "Ahmed"; it will jump to MyString(const char *inputLiterals)
-	//	which is the parametrized constructor.
 
-	//In the previous, I need to call the parametrized constructor only, so I will use member initializer list
+	//1st jump to Employee(int id)
+	//2nd create data members of employeeObj, which will jump to MyString() (Default constructor)
+	//3rd start code of Employee(int id) "Default constructor", and when we say myStringObj = "Ahmed"; it will jump to
+	// MyString(const char *inputLiterals) which is the parametrized constructor.
+
+	//In the previous, I need to call the parametrized constructor only (not default >> parametrized), so I will use member initializer list
 	/* This is how the Employee constructor should be defined:
 	 * Employee(int id): myStringObj(id == 1? "Ahmed" : "Mohamed") {}
 	 */
 
-	/*In general, to convert the mystring data members to be init using member init list:
+	/*In general, to convert the mystring data members to be init using member init list default/parametrized constructors look like that:
 	 * MyString():
 	 * 	literals(nullptr),
 	 * 	size(0)
@@ -305,15 +332,19 @@ int main() {
 	 * }
 	 *
 	 */
-
+	/****************************************************************************************************************************************/
 
 	//3 Ways to construct object from a class
 	//1. MyString studentName;
-	//2. MyString teacherName("Ezz");
-	//3. MyString teacherName = "Ezz";
+	//2. MyString teacherName("Ezz");	 >> This will call parametrized constructor directly.
+	//3. MyString teacherName = "Ezz";   >> This will allocate temporary object with ezz, then copy it to teacherName object using copy constructor
 
+	/****************************************************************************************************************************************/
+	//5. Expicit keyword with constructors:
 	Dummy2 dummy2Obj(10);			//Dummy2 dummy2Obj; will give error, cuz no default constructor
 	func(dummy2Obj);				//func(10); will give error message due to oblige complier not to make implicit casting
+
+	/****************************************************************************************************************************************/
 
 
 
